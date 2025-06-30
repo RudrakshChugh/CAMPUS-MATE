f82e622a43ee0f66296cadc6140bd0b59bb6a3d6
RudrakshChugh <rudrakshchugh005@gmail.com>
2025-06-30 14:43:02 +0530

    .

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,17 @@
-
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBTXb5ggktkl_eSkXKHlAmkRPbjj1YKCO4",
-  authDomain: "acm-inta.firebaseapp.com",
-  projectId: "acm-inta",
-  storageBucket: "acm-inta.appspot.com", // Corrected the storage bucket URL
-  messagingSenderId: "254262352833",
-  appId: "1:254262352833:web:c6c64a9865a4195477ad40"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
 export default app;
